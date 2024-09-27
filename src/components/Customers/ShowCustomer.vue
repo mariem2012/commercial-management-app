c7d14b05ea08c80e1143b0e619faece548e18c23
mariem2012 <mariemdianibafa00@gmail.com>
2024-09-27 10:23:54 +0000

    Added  ShowCustomer component

--- src/components/Customers/ShowCustomer.vue
+++ src/components/Customers/ShowCustomer.vue
@@ -1,5 +1,104 @@
-<script setup></script>
+<template>
+  <div>
+    <div
+      class="modal fade show mt-5"
+      style="display: block"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="customerDetailModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="customerDetailModalLabel">
+              View customer
+            </h5>
+            <button type="button" class="btn-close" @click="closeModal">
+              <span aria-hidden="true"></span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group d-flex flex-column">
+                <label for="customer-name" class="col-form-label"
+                  >Customer Name:</label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="customer.name"
+                  id="customer-name"
+                  disabled
+                />
+              </div>
+              <div class="form-group">
+                <label for="customer-address" class="col-form-label"
+                  >Address:</label
+                >
+                <textarea
+                  class="form-control"
+                  v-model="customer.address"
+                  id="customer-address"
+                  disabled
+                ></textarea>
+              </div>
+              <div class="form-group">
+                <label for="customer-email" class="col-form-label"
+                  >Email:</label
+                >
+                <input
+                  type="email"
+                  class="form-control"
+                  v-model="customer.email"
+                  id="customer-email"
+                  disabled
+                />
+              </div>
+              <div class="form-group">
+                <label for="customer-phone" class="col-form-label"
+                  >Phone:</label
+                >
+                <input
+                  type="tel"
+                  class="form-control"
+                  v-model="customer.phone"
+                  id="customer-phone"
+                  disabled
+                />
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" @click="closeModal">
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
-<template></template>
+<script setup>
+import { defineProps, defineEmits } from "vue";
 
-<style scoped></style>
+const props = defineProps({
+  customer: {
+    type: Object,
+    required: true,
+  },
+});
+
+const emit = defineEmits();
+
+const closeModal = () => {
+  emit("close");
+};
+</script>
+
+<style scoped>
+.close {
+  margin-right: 20px;
+}
+</style>
