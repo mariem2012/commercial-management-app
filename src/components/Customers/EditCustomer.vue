bc130f3fa656c968a068615a972ca6a57f5fbcd9
mariem2012 <mariemdianibafa00@gmail.com>
2024-09-27 10:18:56 +0000

    Added  EditCustomer component

--- src/components/Customers/EditCustomer.vue
+++ src/components/Customers/EditCustomer.vue
@@ -1,5 +1,109 @@
-<script setup></script>
+<template>
+  <div>
+    <div
+      class="modal fade show mt-5"
+      style="display: block"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="editCustomerModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="editCustomerModalLabel">
+              Edit customer
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              @click="closeModal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true"></span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form @submit.prevent="editCustomer">
+              <div class="form-group">
+                <label for="customer-name" col-form-label mb-2
+                  >Customer Name:</label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="customer.name"
+                  id="customer-name"
+                  required
+                />
+              </div>
+              <div class="form-group">
+                <label for="customer-address">Adress:</label>
+                <textarea
+                  class="form-control mt-2"
+                  v-model="customer.address"
+                  id="customer-address"
+                  required
+                ></textarea>
+              </div>
+              <div class="form-group">
+                <label for="customer-email">Email:</label>
+                <input
+                  type="email"
+                  class="form-control mt-2"
+                  v-model="customer.email"
+                  id="customer-email"
+                  required
+                />
+              </div>
+              <div class="form-group d-flex flex-column">
+                <label for="customer-phone">Phone:</label>
+                <input
+                  type="tel"
+                  class="form-control mt-2"
+                  v-model="customer.phone"
+                  id="customer-phone"
+                  required
+                />
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" @click="closeModal">
+              Close
+            </button>
+            <button
+              type="submit"
+              class="btn btn-primary"
+              @click.prevent="editCustomer"
+            >
+              Confirm
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
-<template></template>
+<script setup>
+import { defineProps, defineEmits } from "vue";
 
-<style scoped></style>
+const props = defineProps({
+  customer: {
+    type: Object,
+    required: true,
+  },
+});
+
+const emit = defineEmits();
+
+const closeModal = () => {
+  emit("close");
+};
+
+const editCustomer = () => {
+  emit("edit", { ...props.customer });
+  closeModal();
+};
+</script>
